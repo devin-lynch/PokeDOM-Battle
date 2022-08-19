eaa63391427c432d85b8a8cfceba7781295efdd1
devin-lynch <devin.l.lynch@gmail.com>
2022-08-19 12:26:29 -0500

    Add attack buttons and sprites

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 console.log('hello from js')
+
+let playerHealth = 100
+let cpuHealth = 100
+
+// Need to determine if using 'real' pokemon or something else for characters (like dog vs mailman)
+// Can then decide on attacks
+
+// MVP cpu has one attack -- can add more after battle is working. Player will have 2-4 to choose from. 
+
+// Stretch goal: make an ability that enhances the next turns ability?
+
+
+// Mankey moves
+function lowKick() {
+    cpuHealth - 20
+}
+
+function scratch() {
+    cpuHealth - 25
+}
+
+function closeCombat() {
+    cpuHealth - 30
+}
+
+
+// Dragonite moves
+function hyperBeam() {
+    playerHealth - 25
+}
+
+function dragonRush() {
+    playerHealth - 20
+}
+
+
+function attack() {
+    
+}
+
+
+
+//message if player wins
+function victory() {
+    if (cpuHealth <= 0) { 
+
+    }
+}
+
+//message if cpu wins
+function defeat() {
+    if (playerHealth <= 0) {
+
+    }
+}
+
