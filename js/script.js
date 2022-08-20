51cb946999e02258454882ffd83e35b44fb7d0d7
devin-lynch <devin.l.lynch@gmail.com>
2022-08-20 16:43:16 -0500

    Debug computer random attack

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const textBar = document.querySelector('.textbar')
 
 let playerHealth = 100
 let cpuHealth = 100
+let turn = 0
 
 // Need to determine if using 'real' pokemon or something else for characters (like dog vs mailman)
 // Can then decide on attacks
@@ -27,40 +28,45 @@ attackFour.addEventListener('click', skullBash)
 function lowKick() {
     cpuHealth -= 20
     console.log(`Mankey used Low Kick!`)
-    console.log(cpuHealth)
+    console.log(`Dragonite has ${cpuHealth}hp!`)
     victory() // checks if cpu HP is 0
+    turn = 1
 }
 
 function scratch() {
     cpuHealth -= 25
     console.log(`Mankey used Scratch!`)
-    console.log(cpuHealth)
+    console.log(`Dragonite has ${cpuHealth}hp!`)
     victory() // checks if cpu HP is 0
+    turn = 1
 }
 
 function closeCombat() {
     cpuHealth -= 30
     console.log(`Mankey used Close Combat!`)
-    console.log(cpuHealth)
+    console.log(`Dragonite has ${cpuHealth}hp!`)
     victory() // checks if cpu HP is 0
+    turn = 1
 }
 
 function skullBash() {
     cpuHealth -= 20
     console.log(`Mankey used Skull Bash!`)
-    console.log(cpuHealth)
+    console.log(`Dragonite has ${cpuHealth}hp!`)
     victory() // checks if cpu HP is 0
+    turn = 1
 }
 
 
 
 // Dragonite moves
 
-const cpuAttacks = [hyperBeam(), dragonRush(), hurricane(), thunderPunch()]
+const cpuAttacks = [hyperBeam, dragonRush, hurricane, thunderPunch]
 
 function dragoniteAttack() {
     const random = Math.floor(Math.random() * 4);
-    cpuAttacks[random]
+    cpuAttacks[random]()
+    console.log(`Mankey has ${playerHealth}hp!`)
     // console.log(cpuAttacks[random])
 } 
 
@@ -69,29 +75,29 @@ function dragoniteAttack() {
 function hyperBeam() {
     playerHealth -= 25
     console.log(`The wild Dragonite used Hyper Beam!`)
-    console.log(playerHealth)
     defeat() // checks if user HP is 0
+    turn = 0
 }
 
 function dragonRush() {
     playerHealth -= 20
     console.log(`The wild Dragonite used Dragon Rush!`)
-    console.log(playerHealth)
     defeat() // checks if user HP is 0
+    turn = 0
 }
 
 function hurricane() {
     playerHealth -= 35
     console.log(`The wild Dragonite used Hurricane!`)
-    console.log(playerHealth)
     defeat() // checks if user HP is 0
+    turn = 0
 }
 
 function thunderPunch() {
     playerHealth -= 20
     console.log(`The wild Dragonite used Thunder Punch!`)
-    console.log(playerHealth)
     defeat() // checks if user HP is 0
+    turn = 0
 }
 
 dragoniteAttack()
